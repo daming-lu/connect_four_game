22597aec30a3c3f5a1d63fc8d21452799c4024d1
Daming <daminglu@live.com>
2015-02-13 00:51:47 -0800

    can place discs

--- scripts/connect-four.js
+++ scripts/connect-four.js
@@ -1,12 +1,20 @@
 /**
  * Created by daming on 2/12/15.
  */
+var tokens = 0;
+
+var height = 200;
 
 $(document).ready(function() {
     $(".column-button").click(function() {
-        var index = $(this).parent().parent().index();
+        var index = $(this).parent().index();
         // 0,1,2,...,6
-        alert('index : '+index);
+        //alert('index : '+index);
+        var p = "player2"
+        var newToken = "<div style=\"top:" + height +  "px\" class=\"disc " + p + "\"></div>";
+        height -= 50;
+        //alert(newToken);
+        $(this).prev().prepend(newToken);
         //if(!updateGame(index)) return;
         //started = true;
         //var p = tokens%2 ? "player2" : "player1";
